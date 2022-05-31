7320a9d3ea30f13fd24394edbaca50e6c48541f4
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2022-05-31 00:29:50 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,2 @@
-# Writing to a file in Python
-# This was tested on Python 2.7.
-
-# path - is a string to desired path location. The file does
-#         not have to exist.
-# content - is a string with the file content.
-def writeToFile(path, content):
-  file = open(path, "w")
-  file.write(content)
-  file.close()
-  
-  
-PATH_TO_MY_FILE = './prueba.txt'
-CONTENT_FOR_MY_FILE = 'Example\nThis is on line 2 of a text file.\n\nThe end.'
-
-writeToFile(PATH_TO_MY_FILE, CONTENT_FOR_MY_FILE)
-
-# Run in terminal using:
-# python write-to-file.py
-#
-# It will create a file called `example.txt` and the contents will look like (minus the number signs):
-# Example
-# This is on line 2 of a text file.
-# 
-# The end.
+with open('https://github.com/Mtrx77/rzt/blob/main/prueba.txt','w') as writefile:
+    writefile.write("This is line A")
