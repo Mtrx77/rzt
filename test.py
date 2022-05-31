b8aba97e44fe1bc65ca8c4da227f18768b3f542c
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2022-05-31 00:05:26 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-archivo = open(https://raw.githubusercontent.com/Mtrx77/rzt/main/prueba.txt,"w")
+archivo = open("https://raw.githubusercontent.com/Mtrx77/rzt/main/prueba.txt","w")
 archivo.write("xd")
 archivo.close()
