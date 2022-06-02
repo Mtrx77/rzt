69736c96f3b79dbb037cfe510d50b97095e369b0
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2022-06-02 01:23:44 -0500

    Create chat.lua

---
+++ chat.lua
@@ -0,0 +1,116 @@
+                        bc = BrickColor.new("Lime green")
+                        
+                        game.StarterGui:SetCore("ChatMakeSystemMessage", {
+                        
+                        Text = "[Scripter][Matrixplayer]: Hi there";
+                        
+                        Font = Enum.Font.Highway;
+                        
+                        Color = bc.Color;
+                        
+                        FontSize = Enum.FontSize.Size8;
+                        })
+                        wait(3)
+                        bc = BrickColor.new("Lime green")
+                        
+                        game.StarterGui:SetCore("ChatMakeSystemMessage", {
+                        
+                        Text = "[Scripter][Matrixplayer]: Well here is what many of you have been asking for, the chakra script that massively increases power.";
+                        
+                        Font = Enum.Font.Highway;
+                        
+                        Color = bc.Color;
+                        
+                        FontSize = Enum.FontSize.Size8;
+                        })
+                        wait(7)
+                        bc = BrickColor.new("Lime green")
+                        
+                        game.StarterGui:SetCore("ChatMakeSystemMessage", {
+                        
+                        Text = "[Scripter][Matrixplayer]: You can find it on the training page, in the chakra section.";
+                        
+                        Font = Enum.Font.Highway;
+                        
+                        Color = bc.Color;
+                        
+                        FontSize = Enum.FontSize.Size8;
+                        })
+                        wait(5)
+                        bc = BrickColor.new("Toothpaste")
+                        
+                        game.StarterGui:SetCore("ChatMakeSystemMessage", {
+                        
+                        Text = "[Scripter][Matrixplayer]: I have several things to comment about this feature.";
+                        
+                        Font = Enum.Font.Highway;
+                        
+                        Color = bc.Color;
+                        
+                        FontSize = Enum.FontSize.Size8;
+                        })
+                        wait(5)
+                        bc = BrickColor.new("New Yeller")
+                        
+                        game.StarterGui:SetCore("ChatMakeSystemMessage", {
+                        
+                        Text = "[Scripter][Matrixplayer]: Basically the best thing you can do is to try in which chakra zone it works best for you, for some it works in the spawn, for others it doesn't, it depends on your internet connection.";
+                        
+                        Font = Enum.Font.Highway;
+                        
+                        Color = bc.Color;
+                        
+                        FontSize = Enum.FontSize.Size8;
+                        })
+                        wait(9)
+                        bc = BrickColor.new("New Yeller")
+                        
+                        game.StarterGui:SetCore("ChatMakeSystemMessage", {
+                        
+                        Text = "[Scripter][Matrixplayer]: From my own experience i can say that the chakra zone of 1 UD always works, so if your chakra is greater than 1 UD, try there.";
+                        
+                        Font = Enum.Font.Highway;
+                        
+                        Color = bc.Color;
+                        
+                        FontSize = Enum.FontSize.Size8;
+                        })
+                        wait(5)
+                        bc = BrickColor.new("Really red")
+                        
+                        game.StarterGui:SetCore("ChatMakeSystemMessage", {
+                        
+                        Text = "[Scripter][Matrixplayer]: Also keep in mind that if it doesn't work, change the server and try again, if you don't change the server it probably won't work.";
+                        
+                        Font = Enum.Font.Highway;
+                        
+                        Color = bc.Color;
+                        
+                        FontSize = Enum.FontSize.Size8;
+                        })
+                        wait(6)
+                        bc = BrickColor.new("Really red")
+                        
+                        game.StarterGui:SetCore("ChatMakeSystemMessage", {
+                        
+                        Text = "[Scripter][Matrixplayer]: Be patient it will probably take you many tries.";
+                        
+                        Font = Enum.Font.Highway;
+                        
+                        Color = bc.Color;
+                        
+                        FontSize = Enum.FontSize.Size8;
+                        })
+                        wait(6)
+                        bc = BrickColor.new("Lime green")
+                        
+                        game.StarterGui:SetCore("ChatMakeSystemMessage", {
+                        
+                        Text = "[Scripter][Matrixplayer]: Well that's all for now ;3";
+                        
+                        Font = Enum.Font.Highway;
+                        
+                        Color = bc.Color;
+                        
+                        FontSize = Enum.FontSize.Size8;
+                        })
