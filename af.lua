5387ca0b7a124961035dd94a0356a051ccfb24b9
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2022-06-02 01:59:01 -0500

    Create af.lua

---
+++ af.lua
@@ -0,0 +1,12 @@
+bc = BrickColor.new(0, 255, 242)
+
+game.StarterGui:SetCore("ChatMakeSystemMessage", {
+
+Text = "[Scripter][Matrixplayer]: As its name says, it allows you to farm mobs in dim 1 and 2, it has no more mystery lol.";
+
+Font = Enum.Font.Highway;
+
+Color = bc.Color;
+
+FontSize = Enum.FontSize.Size8;
+})
