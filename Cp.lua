61fb85cb64bea9a5db9a92c0af0196fb0b6031e6
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2022-06-02 01:56:27 -0500

    Create Cp.lua

---
+++ Cp.lua
@@ -0,0 +1,50 @@
+bc = BrickColor.new(0, 255, 242)
+
+game.StarterGui:SetCore("ChatMakeSystemMessage", {
+
+Text = "[Scripter][Matrixplayer]: First of all the fast chakra function is in testing, sometimes it usually gives more increases and sometimes not, after it finishes giving the increases, wait a while to use the function again or you can also rejoin.";
+
+Font = Enum.Font.Highway;
+
+Color = bc.Color;
+
+FontSize = Enum.FontSize.Size8;
+})
+wait(10)
+      
+bc = BrickColor.new("Really red")
+
+game.StarterGui:SetCore("ChatMakeSystemMessage", {
+
+Text = "[Scripter][Matrixplayer]: those who have the (Chakra Healing) gamepass will not be able to use the Chakra Op function. ";
+
+Font = Enum.Font.Highway;
+
+Color = bc.Color;
+
+FontSize = Enum.FontSize.Size8;
+})
+wait(10)
+bc = BrickColor.new(0, 255, 242)
+game.StarterGui:SetCore("ChatMakeSystemMessage", {
+    
+    Text = "[Scripter][Matrixplayer]: apart from that the script works fine and shouldn't have any problems. ";
+    
+    Font = Enum.Font.Highway;
+    
+    Color = bc.Color;
+    
+    FontSize = Enum.FontSize.Size8;
+    })
+    wait(8)
+        bc = BrickColor.new("Lime green")
+        game.StarterGui:SetCore("ChatMakeSystemMessage", {
+
+        Text = "[Scripter][Matrixplayer]: That's all for now, have a nice day OwO";
+        
+        Font = Enum.Font.Highway;
+        
+        Color = bc.Color;
+        
+        FontSize = Enum.FontSize.Size8;
+        })
