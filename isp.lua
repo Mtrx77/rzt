f7cf88c11c2f311ddea6d2a284a5b3bd00235c9a
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2022-06-02 02:02:49 -0500

    Create isp.lua

---
+++ isp.lua
@@ -0,0 +1,25 @@
+bc = BrickColor.new(0, 255, 242)
+
+game.StarterGui:SetCore("ChatMakeSystemMessage", {
+
+Text = "[Scripter][Matrixplayer]: This feature will allow you to get infinite money, boost, chikara and chests as long as you are in a dungeon, if you run the script when you are not in a dungeon, it will automatically teleport you to a random dungeon.";
+
+Font = Enum.Font.Highway;
+
+Color = bc.Color;
+
+FontSize = Enum.FontSize.Size8;
+})
+wait(8)
+bc = BrickColor.new(0, 255, 242)
+
+game.StarterGui:SetCore("ChatMakeSystemMessage", {
+
+Text = "[Scripter][Matrixplayer]: Well that's it, cya.";
+
+Font = Enum.Font.Highway;
+
+Color = bc.Color;
+
+FontSize = Enum.FontSize.Size8;
+})
